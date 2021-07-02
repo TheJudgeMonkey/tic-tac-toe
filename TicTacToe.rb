9882d2e425f7fa66eba93242d5a52865b7a4cb66
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-07-02 18:28:31 +0300

    Add TicTacToe.rb

---
+++ TicTacToe.rb
@@ -0,0 +1,10 @@
+require './prog/game.rb'
+
+puts " "
+puts "------------------------------"
+puts " Welcome to TIC TAC TOE game!"
+puts "------------------------------"
+puts " "
+
+game = Game.new
+game.play  
